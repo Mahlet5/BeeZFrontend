b2603e3c9a6fe811460f8d7a9fd8d007a16c7334
mahlet5 <mahletgirma25@gmail.com>
2020-05-27 21:38:30 +0300

    added_header_component

---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container>
+ <v-toolbar flat>
+   
+      <v-toolbar-title>BeeZ Social ERP</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+     <v-toolbar-items>
+      <v-btn text>
+      HOME
+      </v-btn>
+
+      <v-btn text>
+       VISION
+      </v-btn>
+
+      <v-btn text>
+      MANIFESTO
+      </v-btn>
+      
+      <v-btn text>
+       PRODUCTS
+      </v-btn>
+      
+      <v-btn text>
+        TEAM
+      </v-btn>
+
+      <v-btn text>
+       CONTACT US
+      </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+   </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+
+    data: () => ({
+    
+    }),
+  }
+</script>
